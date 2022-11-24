e27eb2dcd8af959d331b9a8340eb0781cd3905ec
DuDev <Dujardin.univalle@gmail.com>
2022-11-23 21:17:08 -0500

    Añadida plantilla para exportacion

---
+++ old/src/components/Inicio.js
@@ -0,0 +1,18 @@
+export const Inicio = () => {
+    return (
+        <div className="inicio">    
+            <h1>Sistema de ventas e inventario</h1>
+            <h1>Proyecto final</h1>
+            <hr />
+            <h1>SystemPlus - 2022</h1>
+            <p>
+                Sistema de ventas e inventario creado con tecnologías de vanguardia. 
+                <code> React JS</code> para construir un SPA en el front, <code>Node JS</code> para crear una API en donde se exponen endpoints para consumir e insertar datos desde y hacia una base de datos creada en <code>Postgresql - Supabase</code>.
+            </p>
+            <p className="text-center">
+                Creado por Victor Ivan Velez Lopez para el tecnico de Desarrollo de aplicaciones web y Mantenimiento de Computadoras - Systemplus, Colombia.
+                <div className="code"><code className="code"><strong>2022</strong></code></div>
+            </p>
+        </div>
+    )
+}
